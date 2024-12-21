a3fae7c7800896a25db7528843e11444cb8e0367
officialusage01 <officialusage01@proton.me>
2024-12-21 22:08:23 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          400: '#c084fc',
+          500: '#a855f7',
+          700: '#7e22ce',
+          800: '#6b21a8',
+          900: '#581c87',
+        }
+      }
+    },
   },
   plugins: [],
 };
